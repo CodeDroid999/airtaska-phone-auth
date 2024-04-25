91fe7f49c3fb739db55eb46e573cc9cf3f0a52cc
CodeDroid999 <johnmwega2002@gmail.com>
2024-04-25 18:30:49 +0300

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAavfuOUEaH6LFCXLaeCFFRj0hzZBS7X5Y",
+  authDomain: "airtaska-auth-service.firebaseapp.com",
+  projectId: "airtaska-auth-service",
+  storageBucket: "airtaska-auth-service.appspot.com",
+  messagingSenderId: "1055034343573",
+  appId: "1:1055034343573:web:3b2b012ae1ef4cb4241b9c",
+  measurementId: "G-9LER5KR37J",
 };
 
 // Initialize Firebase
