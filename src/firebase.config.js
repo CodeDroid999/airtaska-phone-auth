919b845b273df36a6b76696b0ceb83b903a86319
CodeDroid999 <johnmwega2002@gmail.com>
2024-04-26 11:26:04 +0300

    build(deps): update package-lock.json, package.json and firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAavfuOUEaH6LFCXLaeCFFRj0hzZBS7X5Y",
-  authDomain: "airtaska-auth-service.firebaseapp.com",
-  projectId: "airtaska-auth-service",
-  storageBucket: "airtaska-auth-service.appspot.com",
-  messagingSenderId: "1055034343573",
-  appId: "1:1055034343573:web:3b2b012ae1ef4cb4241b9c",
-  measurementId: "G-9LER5KR37J",
+  apiKey: "AIzaSyBhlvPkdYdEmByHhdsTLVrASxAqP2pudjI",
+  authDomain: "airtaska-authentication.firebaseapp.com",
+  projectId: "airtaska-authentication",
+  storageBucket: "airtaska-authentication.appspot.com",
+  messagingSenderId: "480731115943",
+  appId: "1:480731115943:web:96ef8da56cb97282d80682",
+  measurementId: "G-P2NEXHW6X0",
 };
 
 // Initialize Firebase
