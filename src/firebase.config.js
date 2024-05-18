61cd82b772683f3a95e51dfdcab4d75d1284b7b7
CodeDroid999 <johnmwega2002@gmail.com>
2024-05-18 15:57:25 +0300

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,14 +7,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9DMyKKlKHxybCZqImW_Qt9V14lIWxSTc",
-  authDomain: "northexpresskenya.firebaseapp.com",
-  databaseURL: "https://northexpresskenya-default-rtdb.firebaseio.com",
-  projectId: "northexpresskenya",
-  storageBucket: "northexpresskenya.appspot.com",
-  messagingSenderId: "498415498621",
-  appId: "1:498415498621:web:0cc11cd5435df214edc814",
-  measurementId: "G-MQXNWGXLNZ",
+  apiKey: "AIzaSyCAqmqgLkNxnzblwUIKI0ikeV4YfPT1ZmQ",
+  authDomain: "computershop-254.firebaseapp.com",
+  databaseURL: "https://computershop-254-default-rtdb.firebaseio.com",
+  projectId: "computershop-254",
+  storageBucket: "computershop-254.appspot.com",
+  messagingSenderId: "90702954375",
+  appId: "1:90702954375:web:6ea04360703fc61e2a1d22",
+  measurementId: "G-STY9N4E6BS",
 };
 
 // Initialize Firebase
