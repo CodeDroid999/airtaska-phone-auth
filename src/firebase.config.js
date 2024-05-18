97f322d2a04c1eb8088a6f9410a7641503fe5e77
CodeDroid999 <johnmwega2002@gmail.com>
2024-05-18 21:05:47 +0300

    build(deps): update package-lock.json, package.json and firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,19 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { config } from "dotenv";
+
+// Load environment variables from .env file
+config();
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDn2tkJGDo60RyMz-1oUFIaWJw2Xevoyc",
-  authDomain: "airtaska-authenticate.firebaseapp.com",
-  projectId: "airtaska-authenticate",
-  storageBucket: "airtaska-authenticate.appspot.com",
-  messagingSenderId: "756623350641",
-  appId: "1:756623350641:web:2c6eb9b56250114354f4d0",
-  measurementId: "G-7JBQC82GVP",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
