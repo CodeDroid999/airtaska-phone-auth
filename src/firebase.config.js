fc659889f295c7b911a3c902ad6051a6eaf1e06e
CodeDroid999 <johnmwega2002@gmail.com>
2024-05-18 19:56:14 +0300

    update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,14 +7,13 @@ import { getAuth, signOut } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAqmqgLkNxnzblwUIKI0ikeV4YfPT1ZmQ",
-  authDomain: "computershop-254.firebaseapp.com",
-  databaseURL: "https://computershop-254-default-rtdb.firebaseio.com",
-  projectId: "computershop-254",
-  storageBucket: "computershop-254.appspot.com",
-  messagingSenderId: "90702954375",
-  appId: "1:90702954375:web:6ea04360703fc61e2a1d22",
-  measurementId: "G-STY9N4E6BS",
+  apiKey: "AIzaSyAXWWK4eDca27Ywrv5wb5s0XV7FOSE1kMY",
+  authDomain: "airtaska-18e8a.firebaseapp.com",
+  projectId: "airtaska-18e8a",
+  storageBucket: "airtaska-18e8a.appspot.com",
+  messagingSenderId: "994864550546",
+  appId: "1:994864550546:web:8b3ada161b3584e4454c4b",
+  measurementId: "G-MJ1D6YPN8K",
 };
 
 // Initialize Firebase
